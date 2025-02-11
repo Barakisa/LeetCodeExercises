b9cdb7195dd041da66aa109bb3ad9c97c3389661
Nojus <nojus.dziaugys@gmail.com>
2025-02-11 17:34:40 +0200

    1910 full solution

---
+++ 1910. Remove All Occurrences of a Substring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Contracts;
+using System.IO;    
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var sol = new Solution();
+        var res = sol.RemoveOccurences("daabcbaabcbc", 0, "abc");
+        Console.WriteLine(res);
+    }
+}
+
+public class Solution
+{
+    public string RemoveOccurences(String s, int start, String part)
+    {
+        while(s.Contains(part)){
+            var index = s.IndexOf(part);
+            s = s.Remove(index, part.Length);
+        }
+        return s;
+    }
+}
